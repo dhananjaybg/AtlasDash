4cc660b7d2e963cea85736b533a4003cb60b58e2
Dhananjay Ghevde <dhananjaybg@gmail.com>
2023-03-02 22:30:39 -0500

    Update setupAtlasDashAppService.ps1

--- setupAtlasDashAppService.ps1
+++ setupAtlasDashAppService.ps1
@@ -5,14 +5,10 @@ $ErrorActionPreference = "Stop"
 Write-Output "
 /***
  *                                                                               
- *     _____                 ____  _____    _____ _ _ _ _            ____          _   _                 _
- *    |     |___ ___ ___ ___|    \| __  |  | __  |_| | |_|___ ___   |    \ ___ ___| |_| |_ ___ ___ ___ _| |___
- *    | | | | . |   | . | . |  |  | __ -|  | __ -| | | | |   | . |  |  |  | .'|_ -|   | . | . | .'|  _| . |_ -|
- *    |_|_|_|___|_|_|_  |___|____/|_____|  |_____|_|_|_|_|_|_|_  |  |____/|__,|___|_|_|___|___|__,|_| |___|___|
- *                  |___|                                    |___|               
+ *              
  */
 "
-Write-Output "This script configures a MongoDB App Services app which extracts data from the Atlas Billing API"
+Write-Output "This script configures a MongoDB App Services app which extracts data from the Atlas DASH API"
 Write-Output "and writes it to an Atlas Cluster`n"
 
 Write-Output "Before you run this script, make sure you have:"
@@ -61,17 +57,17 @@ realm-cli import --yes
 realm-cli secrets create -n billing-orgSecret -v $orgID
 realm-cli secrets create -n billing-usernameSecret -v $publicKeyOrg
 realm-cli secrets create -n billing-passwordSecret -v $privateKeyOrg
-realm-cli push --remote "billing" -y
+realm-cli secrets create -n database-secret -v "AtlasDash"
+realm-cli push --remote "AtlasDash" -y
 
 # Run functions to retrieve billing data for the first time
 Write-Output "Please wait a few seconds before we run the getAll function ..."
 Start-Sleep 30
-realm-cli function run --name "getAll"
-Write-Output "Please wait a few seconds before we run the processAll function ..."
-realm-cli function run --name "processAll"
+realm-cli function run --name "start_process"
+
 
 # Next Steps
 Write-Output "Setup Complete! Please log into Atlas and verify that data has been loaded into the cluster."
-Write-Output "To visualize the billing data on a dashboard:"
+Write-Output "To visualize the AtlasDash data on a dashboard:"
 Write-Output "1. Activate Charts in your Atlas project"
-Write-Output "2. Import the dashboard from the included file 'charts_billing_template.charts'"
+Write-Output "2. Import the dashboard from the included file 'Travelers Atlas Resource Dashboard.charts'"
