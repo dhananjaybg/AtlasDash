30bfebb89040647ef2b961932ae8dabdc89344b2
Dhananjay Ghevde <dhananjaybg@gmail.com>
2023-03-02 22:27:43 -0500

    Update setupAtlasDashAppService.sh

--- setupAtlasDashAppService.sh
+++ setupAtlasDashAppService.sh
@@ -3,21 +3,17 @@
 echo "
 /***
  *                                                                                                             
- *     _____                 ____  _____    _____ _ _ _ _            ____          _   _                 _     
- *    |     |___ ___ ___ ___|    \| __  |  | __  |_| | |_|___ ___   |    \ ___ ___| |_| |_ ___ ___ ___ _| |___ 
- *    | | | | . |   | . | . |  |  | __ -|  | __ -| | | | |   | . |  |  |  | .'|_ -|   | . | . | .'|  _| . |_ -|
- *    |_|_|_|___|_|_|_  |___|____/|_____|  |_____|_|_|_|_|_|_|_  |  |____/|__,|___|_|_|___|___|__,|_| |___|___|
- *                  |___|                                    |___|                                             
+ * Mongo Atlas Dash   
+ *
  */
 "
-
 
 echo "This script configures a MongoDB App Services app which extracts data from the Atlas Billing API"
 echo "and writes it to an Atlas Cluster"
 echo 
 echo "Before you run this script, make sure you have:"
-echo "1. Created a new MongoDB Atlas project for your billing app"
-echo "2. Created a new cluster inside that project for storing billing data"
+echo "1. Created a new MongoDB Atlas project for your AtlasDash app"
+echo "2. Created a new cluster inside that project for storing AtlasDash data"
 echo "3. Created an API Key inside that project, and recorded the public and private key details"
 echo "4. Created an API Key for your Organization and recorded the public and private key details"
 echo "5. Installed dependencies for this script: node, mongodb-realm-cli"
@@ -33,7 +29,7 @@ echo "Enter the PUBLIC Key for your ORGANIZATION level API Key:"
 read publicKeyOrg
 echo "Enter the PRIVATE Key for your ORGANIZATION level API Key:"
 read privateKeyOrg
-echo "Enter the name of the running Atlas cluster that will store the billing data:"
+echo "Enter the name of the running Atlas cluster that will store the AtlasDash data:"
 read clusterName
 
 echo "Thanks....."
@@ -59,19 +55,20 @@ realm-cli login --api-key="$publicKeyProject" --private-api-key="$privateKeyProj
 realm-cli import --yes 
 
 # Write secrets to App Services app
-realm-cli secrets create -n billing-orgSecret -v $orgID
-realm-cli secrets create -n billing-usernameSecret -v $publicKeyOrg
-realm-cli secrets create -n billing-passwordSecret -v $privateKeyOrg
-realm-cli push --remote "billing" -y
+realm-cli secrets create -n org-secret -v $orgID
+realm-cli secrets create -n username-secret -v $publicKeyOrg
+realm-cli secrets create -n password-secret -v $privateKeyOrg
+realm-cli secrets create -n database-secret -v "AtlasDash"
+realm-cli push --remote "AtlasDash" -y
 
 # Run functions to retrieve billing data for the first time
 echo "Please wait a few seconds before we run the getAll function ..."
 
 sleep 30
-realm-cli function run --name "getAll"
+realm-cli function run --name "start_process"
 
-echo "Please wait a few seconds before we run the processAll function ..."
-realm-cli function run --name "processAll"
+#echo "Please wait a few seconds before we run the processAll function ..."
+#realm-cli function run --name "processAll"
 
 # Next Steps
 echo "Setup Complete! Please log into Atlas and verify that data has been loaded into the cluster."
